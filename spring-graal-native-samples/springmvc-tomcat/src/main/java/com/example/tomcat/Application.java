72f98504b111427a35211ac34969cbdc3ee76e37
Murray Tait <murray.tait@infinityworks.com>
2020-01-07 09:46:47 +0000

    Clean imports

--- spring-graal-native-samples/springmvc-tomcat/src/main/java/com/example/tomcat/Application.java
+++ spring-graal-native-samples/springmvc-tomcat/src/main/java/com/example/tomcat/Application.java
@@ -1,17 +1,10 @@
-package com.example.tomcat;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+	package com.example.tomcat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication(proxyBeanMethods = false)
